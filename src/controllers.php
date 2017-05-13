34203011f53a0c9a6d494b1962b06d773021c553
josmrhyde <iherjos@comunidad.unam.mx>
2017-05-13 11:28:46 -0500

    Add files via upload
    13Mayo2017

--- src/controllers.php
+++ src/controllers.php
@@ -4,20 +4,36 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Session\Storage\Handler\NativeFileSessionHandler;
+use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorage;
+
 use Symfony\Component\Validator\Constraints as Assert;
-use Silex\Application\TwigTrait;
-use Application\UrlGeneratorTrait;
 //Request::setTrustedProxies(array('127.0.0.1'));
 
-$app->get('/', function () use ($app) {
-    return $app['twig']->render('index.html.twig', array());
-})
-->bind('homepage')
-;
+$app->get('/', function (Request $request) use ($app) {
+    if (null === $user = $app['session']->get(null)) {
+        return $app['twig']->render('index.html.twig', array('usuario'=>$app['session']->get('user'),
+        'correo'=>$app['session']->get('email')));
+    }
+    if (null === $user = $app['session']->get('user')) {
+        return $app->redirect($app['url_generator']->generate('login_vista'));
+    }
+
+})->bind('homepage');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
 $app->get('/blog/{id}', function ($id) use ($app) {
     $sql = "SELECT * FROM clientes WHERE id_Cliente = $id";
@@ -37,9 +53,9 @@ $app->get('/blog/{id}', function ($id) use ($app) {
         'telefono'=>$post['Telefono'],
         'fecha_creacion'=>$post['Fecha_creacion']
         ));
-})
-->bind('blog_id')
-;
+
+})->bind('blog_id');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
 $app->get('/registro', function() use ($app) {
     $sql = 'SELECT * FROM Clientes';
@@ -47,6 +63,7 @@ $app->get('/registro', function() use ($app) {
 
     return $app['twig']->render('cliente.html.twig', array());
 })->bind('registrar');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
 $app->post('/registro', function(Request $request) use($app){
     $regcli = $app['db']->insert('Clientes', array(
@@ -87,62 +104,164 @@ $app->post('/registro', function(Request $request) use($app){
         
         return $app->redirect($app['url_generator']->generate('registrar'));
     }
-
 })->bind('registrar_item');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
-$app->get('/productos/', function () use ($app) {
-    /*if (null === $user = $app['session']->get('user')) {
+$app->get('/productos/', function (Request $request) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
         return $app->redirect($app['url_generator']->generate('login_vista'));
-    }*/
+    }
+    
     $sql = "SELECT sku, Categoria_id, Nombre_Producto, Precio, url_imagen_grande FROM Productos";
     $post_productos = $app['db']->fetchAll($sql, array());
     
     return  $app['twig']->render('productos.html.twig', array(
-        'post_productos'=>$post_productos
+        'post_productos'=>$post_productos, 'usuario'=>$user, 'correo'=>$app['session']->get('email')
         ));
-})
-->bind('productos_vista')
-;
+})->bind('productos_vista');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
-$app->get('/productos/:sku/', function () use ($app) {
-    /*if (null === $user = $app['session']->get('user')) {
+$app->get('/productos/:sku/', function (Request $request) use ($app) {
+    if (null === $user = $app['session']->get('user')) {
         return $app->redirect($app['url_generator']->generate('login_vista'));
-    }*/
+    }
+
     $sql = "SELECT sku, Categoria_id, Nombre_Producto, Descripcion, Precio, url_imagen FROM Productos";
     $post_productos = $app['db']->fetchAll($sql, array());
     
     return  $app['twig']->render('productos_sku.html.twig', array(
         'post_productos'=>$post_productos
         ));
-})
-->bind('productos_sku')
-;
+})->bind('productos_sku');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
-$app->post('/agregar/:sku', function(Request $request) use($app){
+$app->post('/agregar/sku', function(Request $request) use($app){
+    //Falta pulir
+    $regped = $app['db']->insert('Carrito', array(
+        'sku' => $request->get('sku'),
+        'Cantidad' => $request->get('cantidad'),
+        'Cliente_id'=> $app['session']->get('user'))
+    );
+    $regpped = $app['db']->insert('Ppedidos', array(
+        'sku' => $request->get('sku'),
+        'Cantidad' => $request->get('cantidad'),
+        'Cliente_id'=> $app['session']->get('user'))
+    );
+    
+    $sqlpped="SELECT * FROM Ppedidos ORDER BY `idPedido` DESC LIMIT 1";
+    $postpped = $app['db']->fetchAssoc($sqlpped);
 
-})->bind('agregar_item')
-;
+    $regpc = $app['db']->insert('PedidosCarrito', array(
+        'idCarrito'=> $app['session']->get('carrito'),
+        'idPedido' => $postpped['idPedido']
+        )
+    );
+    return "Pedido registrado  !";
+
+})->bind('agregar_item');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
+
+$app->get('/carrito', function(Request $request) use ($app){
+    $user = $app['session']->get('user');
+    $car = $app['session']->get('carrito');
+
+    $sqlcheck = "SELECT * FROM Carritos
+        INNER JOIN PedidosCarrito ON Carritos.idCarrito=$car and PedidosCarrito.idCarrito = $car 
+        INNER JOIN Ppedidos On PedidosCarrito.idPedido=Ppedidos.idPedido 
+        INNER JOIN Productos On Ppedidos.sku=Productos.sku";
+    $post_check = $app['db']->fetchAll($sqlcheck, array());
+
+    return $app['twig']->render('carrito.html.twig', array(
+        'post_check'=>$post_check, 'usuario'=>$user, 'correo'=>$app['session']->get('email')));
+})->bind('carrito_vista');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
+
+$app->post('/carrito', function(Request $request) use ($app){
+    $actped = $app['db']->update('Ppedidos', array(
+        'Cantidad' => $request->get('cantidad')),
+        array('idPedido'=>$request->get('idpedido'))
+    );
+    return "Registro actualizado!!!";
+})->bind('carrito_actualizar');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
+
 /*$app->get('/productos/:sku', function (Request $request) use($app){
     return "Detalles: Programar detalles del producto!";
 
     $sku = $request->get('pp.sku');
-    $sql ="SELECT sku, Nombre_Producto, Descripcion, Precio, Categoria_id FROM Productos Where sku=?";
+    $sql ="SELECT * FROM Productos Where sku=?";
     $post = $app['db']->fetchAssoc($sql, array($sku));
 })->bind('producto_detalle');*/
 
-/*$app->get('/login', function(Request $request) use($app){
+$app->match('/form', function (Request $request) use ($app) {
+    // some default data for when the form is displayed the first time
 
-})->bind('login_vista');*/
+    $form = $app['form.factory']->createBuilder(FormType::class)
+        ->add('Correo', TextType::class, array(
+            'constraints' => new Assert\Email()
+        ))
+        ->add('Clave', PasswordType::class, array(
+            'constraints' => array(new Assert\NotBlank(), new Assert\Length(array('min' => 5)))
+        ))
+
+        ->add('billing_plan', ChoiceType::class, array(
+            'choices' => array('Libre' => 1, 'Pequeño negocio' => 2, 'Corporativo' => 3),
+            'expanded' => true,
+            'constraints' => new Assert\Choice(array(1, 2, 3)),
+        ))
+        ->add('fechaVencimiento', DateType::class)
+        ->add('submit', SubmitType::class, [
+            'label' => 'Guardar',
+        ])
+        ->getForm();
+    
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+
+        // do something with the data
+        $sql = "SELECT * FROM Clientes WHERE correo = ?";
+        $post = $app['db']->fetchAssoc($sql, array(strtolower($data['correo'])));
+    
+        if ($post['Correo'] === $username && $post['Clave'] === $password) {
+            //$app['session']->set('user', array('username' => $username));
+            $app['session']->set('user', array('idc'=>$post['id_Cliente']));
+            return $app->redirect($app['url_generator']->generate('productos_vista'));
+        }
+        // redirect somewhere
+        return $app->redirect($app['url_generator']->generate('productos_sku'));
+    }
+
+    // display the form
+    return $app['twig']->render('form.html.twig', array('form' => $form->createView()));
+});
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
 $app->get('/login', function (Request $request) use ($app) {
     $username = $request->server->get('PHP_AUTH_USER', false);
     $password = $request->server->get('PHP_AUTH_PW');
  
     $sql = "SELECT * FROM Clientes WHERE correo = ?";
+    $sqlcar="SELECT * FROM Carritos ORDER BY `idCarrito` DESC LIMIT 1";
     $post = $app['db']->fetchAssoc($sql, array(strtolower($username)));
     
     if ($post['Correo'] === $username && $post['Clave'] === $password) {
-        $app['session']->set('user', array('username' => $username));
+        //  $app['session']->set('user', array('username' => $username));
+        $app['session']->set('user', $post['id_Cliente']);
+        $app['session']->set('email', $post['Correo'] );
+        $sqlcarrito="SELECT * FROM Carritos";
+        $regped = $app['db']->insert('Carritos', array(
+            'Cliente_id'=> $app['session']->get('user'))
+        );
+        
+        if ($regped['idCarrito']==0){
+            $sqlcount = $regped['idCarrito']+1;
+            $postcar = $app['db']->fetchAssoc($sqlcar, array((int)$sqlcount));
+        }else{
+            $postcar = $app['db']->fetchAssoc($sqlcar, array((int)$sqlcount));
+        }
+        $app['session']->set('carrito', $postcar['idCarrito'] );
         return $app->redirect($app['url_generator']->generate('productos_vista'));
     }
 
@@ -150,14 +269,87 @@ $app->get('/login', function (Request $request) use ($app) {
     $response->headers->set('WWW-Authenticate', sprintf('Basic realm="%s"', 'site_login'));
     $response->setStatusCode(401, 'Please sign in.');
     return $response;
+    return "Welcome {$user['username']}!";
 })->bind('login_vista');
+//♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪♪
 
-$app->get('/account', function () use ($app) {
+/*$app->get('/account', function () use ($app) {
     if (null === $user = $app['session']->get('user')) {
-        return $app->redirect('/login');
+        return $app->redirect($app['url_generator']->generate('login_vista'));
     }
 
     return "Welcome {$user['username']}!";
+});*/
+
+$app->get('/logout', function () use ($app) {
+    //session_unset();
+    //session_destroy();
+    //session_start();
+    //$user = $app['session']->set(null,null);
+    //$app['session.storage.handleRequest']=null;
+    //$app['session']->session_destroy();
+    //$app['session.storage.handler'] = null;
+    //'invalidate_session'=>true;
+    //return new Response('logout',200);
+    $user = $app['session']->get(null);
+    return $app->redirect($app['url_generator']->generate('homepage'));
+})->bind('log_out');
+
+$app->match('/formp', function (Request $request) use ($app) {
+    // some default data for when the form is displayed the first time
+    $sql = "SELECT sku, Categoria_id, Nombre_Producto, Descripcion, Precio, url_imagen FROM Productos";
+    $post_productos = $app['db']->fetchAll($sql, array());
+
+    $form = $app['form.factory']->createBuilder(FormType::class)
+        //->add('SKU', TextType::class)
+        //->add('Categoria', TextType::class)
+        //->add('Producto', TextType::class)
+        //->add('Descripcion', TextType::class)
+        //->add('Precio', TextType::class)
+        ->add('Cantidad', IntegerType::class)
+        ->add('submit', SubmitType::class, [
+            'label' => 'Agregar'])
+        ->getForm();
+    
+    $form->handleRequest($request);
+
+    if ($form->isValid() && $form->isSumitted()) {
+        $data = $form->getData();
+
+        // do something with the data
+        if ($post['Correo'] === $username && $post['Clave'] === $password) {
+            //$app['session']->set('user', array('username' => $username));
+            $app['session']->set('user', array('idc'=>$post['id_Cliente']));
+            return $app->redirect($app['url_generator']->generate('productos_vista'));
+        }
+        // redirect somewhere
+        return $app->redirect($app['url_generator']->generate('productos_sku'));
+    }
+
+    // display the form
+    return $app['twig']->render('prueba.html.twig', array('form' => $form->createView(), 'post_productos'=>$post_productos));
+});
+
+$app->get('/p', function(Request $request) use($app){
+    $sql = "SELECT sku, Categoria_id, Nombre_Producto, Descripcion, Precio, url_imagen FROM Productos";
+    $post_productos = $app['db']->fetchAll($sql, array());
+    //$num_productos = $app['db']->count($sql, array());
+    $skus=array();
+    foreach ($post_productos as $sku) {
+        # code...
+        $skus[$sku['sku']]=$sku['Nombre'];
+    }
+    
+    $form = $app['form.factory']->createBuilder(FormType::class)
+    ->add('role','select', array(
+        'selects'   => $skus,
+        'expanded'  => true,)
+    )->getForm();
+
+    return $app['twig']->render('p.html.twig', array('form' => $form->createView(), 'skus'=>$skus));
+    /*return  $app['twig']->render('productos_sku.html.twig', array(
+        'post_productos'=>$post_productos
+        ));*/
 });
 
 $app->error(function (\Exception $e, Request $request, $code) use ($app) {
